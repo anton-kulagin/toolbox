3533eea6359193cc73b001c1b3c408e219578421
Anton <titusja@yandex.ru>
2017-11-18 23:51:17 +0200

    updated to angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { NgFor } from '@angular/common';
+import { NgForOf } from '@angular/common';
 import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { MatSliderModule,MatInputModule,MatButtonModule,MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
